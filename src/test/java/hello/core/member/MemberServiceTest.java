871ccc5c6a807cb7b3e4b2a453e92d2b7b1b6845
문지원 <mon99745@gmail.com>
2022-02-14 18:06:42 +0900

    remark update

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -6,6 +6,14 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+/*
+ *  @Test : 본 어노테이션을 붙이면 Test 메서드로 인식하고 테스트
+ *  JUnit5 기준으로 접근제한자가 Default 여도 된다. (JUnit4 까지는 public이어야 했었다.)
+ *  @BeforeEach : 본 어노테이션을 붙인 메서드는 테스트 메서드 실행 이전에 수행
+ *  @AfterEach : 본 어노테이션을 붙인 메서드는 테스트 메서드 실행 이후에 수행
+ *  @Disabled : 본 어노테이션을 붙인 테스트 메서드는 무시
+ *
+ */
 public class MemberServiceTest {
 
     MemberService memberService;

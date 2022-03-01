1a720e43a47bdf826d6aa3604fa115f9940de7ed
문지원 <mon99745@gmail.com>
2022-03-02 05:02:36 +0900

    Singleton test -update

--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -8,18 +8,24 @@ import org.springframework.context.annotation.Bean;
 public class StatefulServiceTest {
     @Test
     void statefulServiceSingleton() {
-        ApplicationContext ac = new
-                AnnotationConfigApplicationContext(TestConfig.class);
-        StatefulService statefulService1 = ac.getBean("statefulService",
-                StatefulService.class);
-        StatefulService statefulService2 = ac.getBean("statefulService",
-                StatefulService.class);
-//ThreadA: A사용자 10000원 주문 statefulService1.order("userA", 10000);
-//ThreadB: B사용자 20000원 주문 statefulService2.order("userB", 20000);
-//ThreadA: 사용자A 주문 금액 조회
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean("statefulService", StatefulService.class);
+        StatefulService statefulService2 = ac.getBean("statefulService", StatefulService.class);
+
+        //ThreadA: A사용자 10000원 주문
+        statefulService1.order("userA", 10000);
+
+        //ThreadB: B사용자 20000원 주문
+        statefulService2.order("userB", 20000);
+        //ThreadA: 사용자A 주문 금액 조회
         int price = statefulService1.getPrice();
-//ThreadA: 사용자A는 10000원을 기대했지만, 기대와 다르게 20000원 출력 System.out.println("price = " + price);
+
+        //ThreadA: 사용자A는 10000원을 기대했지만, 기대와 다르게 20000원 출력
+        System.out.println("price = " + price);
+
         Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+
+        // 지역변수를 통해서 값을 할당해서 문제를 해결하는 방법이 있다.
     }
     static class TestConfig {
         @Bean

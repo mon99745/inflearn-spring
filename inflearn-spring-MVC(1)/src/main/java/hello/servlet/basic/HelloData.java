d6d79683784f69765c35adf85c7ad12c6ff8cd4e
문지원 <mon99745@gmail.com>
2022-03-16 23:28:44 +0900

    feat (Http)

---
+++ inflearn-spring-MVC(1)/src/main/java/hello/servlet/basic/HelloData.java
@@ -0,0 +1,22 @@
+package hello.servlet.basic;
+
+public class HelloData {
+    private String username;
+    private int age;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}

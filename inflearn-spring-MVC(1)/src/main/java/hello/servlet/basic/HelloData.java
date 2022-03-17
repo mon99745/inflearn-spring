54254d54d9782424bfce975ec7f5f5fddf8e2e24
문지원 <mon99745@gmail.com>
2022-03-18 04:49:52 +0900

    feat (Servlet)

--- inflearn-spring-MVC(1)/src/main/java/hello/servlet/basic/HelloData.java
+++ inflearn-spring-MVC(1)/src/main/java/hello/servlet/basic/HelloData.java
@@ -1,22 +1,13 @@
 package hello.servlet.basic;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class HelloData {
     private String username;
     private int age;
 
-    public String getUsername() {
-        return username;
-    }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
---
+++ inflearn-spring-MVC(1)/src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,31 @@
+package hello.servlet.basic.response;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+/**
+ * http://localhost:8080/response-json
+ *
+ */
+@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+    private ObjectMapper objectMapper = new ObjectMapper();
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse
+            response)
+            throws ServletException, IOException {
+        //Content-Type: application/json
+        response.setHeader("content-type", "application/json");
+        response.setCharacterEncoding("utf-8");
+        HelloData data = new HelloData();
+        data.setUsername("kim");
+        data.setAge(20);
+        //{"username":"kim","age":20}
+        String result = objectMapper.writeValueAsString(data);
+        response.getWriter().write(result);
+    }
+}
